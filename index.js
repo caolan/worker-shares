42040fd999d1651aff427db521fbc04c81942f93
Gregor Martynus <gregor@martynus.net>
2013-01-23 06:12:34 +0100

    inheriting from hoodie-worker

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var Worker = require("./lib/Worker.js");
+var fs = require("fs");
+var Worker = require("./lib/worker.js");
+
+var package_json = JSON.parse(fs.readFileSync("./package.json"));
+// turn 'hoodie-worker-whatever' in 'whatever'
+var workerName   = package_json.name.substr(14);
 
 var config = {
+  name: workerName,
   server: process.env["HOODIE_SERVER"],
   admin: {
     user: process.env["HOODIE_ADMIN_USER"],
