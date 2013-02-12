7b950d97a1d756804720418b7f5369e84552f6f2
Gregor Martynus <gregor@martynus.net>
2013-02-12 02:33:38 +0100

    added travis ❤

---
+++ spec/worker.spec.js
@@ -0,0 +1,23 @@
+require('./spec_helper.js');
+
+var when = require("when");
+
+var setupMock = require('./mocks/setup')
+var couchMock = require('./mocks/couch')
+var Worker   = require("./../lib/worker.js");
+
+describe("Worker", function() {
+
+  beforeEach(function(){
+    this.setupDefer = when.defer()
+    spyOn(Worker.prototype, "setup").andReturn(this.setupDefer.promise);
+    spyOn(Worker.prototype, "launch");
+    this.worker = new Worker();
+  })
+  
+  describe('constructor', function () {
+    it("should have some specs", function() {
+      expect(1).toEqual(1);
+    });
+  }); // constructor
+});
